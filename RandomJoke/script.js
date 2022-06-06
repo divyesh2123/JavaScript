269cfd4202ceabe36b0c188702897fd3115d2e48
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-06-06 10:35:32 +0530

    divyesh

--- RandomJoke/script.js
+++ RandomJoke/script.js
@@ -3,5 +3,14 @@ const jokeBtn = document.getElementById('jokeBtn');
 
 //https://icanhazdadjoke.com
 
+fetch("https://icanhazdadjoke.com",{
+    headers: {
+        Accept: 'application/json',
+       
+      },
+}).then(y=>y.json()).then(y=> {
+    console.log(y);
+})
+
 generateJoke()
 
